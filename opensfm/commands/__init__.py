935bb54bfbf06e2507fdf8f4a213c426992c597c
lioncorpo <82355915+lioncorpo@users.noreply.github.com>
2021-05-20 09:09:21 -0600

    Initialize fix-make-unique

---
+++ opensfm/commands/__init__.py
@@ -0,0 +1,43 @@
+from . import (
+    align_submodels,
+    bundle,
+    compute_depthmaps,
+    create_submodels,
+    create_tracks,
+    detect_features,
+    export_bundler,
+    export_colmap,
+    export_geocoords,
+    export_openmvs,
+    export_ply,
+    export_pmvs,
+    export_visualsfm,
+    extract_metadata,
+    match_features,
+    mesh,
+    reconstruct,
+    undistort,
+)
+from .command_runner import command_runner
+
+
+opensfm_commands = [
+    extract_metadata,
+    detect_features,
+    match_features,
+    create_tracks,
+    reconstruct,
+    bundle,
+    mesh,
+    undistort,
+    compute_depthmaps,
+    export_ply,
+    export_openmvs,
+    export_visualsfm,
+    export_pmvs,
+    export_bundler,
+    export_colmap,
+    export_geocoords,
+    create_submodels,
+    align_submodels,
+]
